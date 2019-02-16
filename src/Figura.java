d946d65f027741ae1a75f82fcdf37f4d764402c4
Willicornio <guillecacerola@gmail.com>
2019-02-16 11:55:24 +0100

    paquete

---
+++ src/Figura.java
@@ -0,0 +1,7 @@
+
+public abstract class Figura {
+
+    private double area;
+
+    public abstract double area();
+}
---
+++ src/GestorFiguras.java
@@ -0,0 +1,20 @@
+import Figura.*;
+public class GestorFiguras {
+
+    public static double suma(Figura[] figuras) {
+
+        double res = 0;
+        for (figuras f : figuras) {
+            res = res + f.area();
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Figura[] f = new Figura[4];
+        f[0] = new Circulo(5.0);
+        f[1] = new Rectangulo(14.3, 17.5);
+
+        System.out.println("suma=" + suma(f));
+    }
+}
