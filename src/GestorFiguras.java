5e07a798cc2d7a1099370d8806919a12b001cd0c
Willicornio <guillecacerola@gmail.com>
2019-02-22 20:41:38 +0100

    Compara y tiene un MainTest que funciona pero con chapuzas.

--- src/GestorFiguras.java
+++ src/GestorFiguras.java
@@ -1,5 +1,4 @@
 import Figura.*;
-import java.util.Arrays;
 
 public class GestorFiguras {
 
@@ -14,18 +13,9 @@ public class GestorFiguras {
 
     public static void main(String[] args) {
 
-        Figura[] f = new Figura[4];
-        f[0] = new Circulo(5.0);
-        f[1] = new Rectangulo(14.3, 17.5);
-        f[2] = new Triangulo(40.2, 5.0);
-        f[3] = new Cuadrado(6.0);
+        System.out.println("No pinto nada aquí, pero si me borran el otro main no funciona");
 
-        System.out.println("suma=" + GestorFiguras.suma(f));
-
-        Arrays.sort(f); //lo ordena de menro a mayor
-        for (Figura i : f) {
-            System.out.println(i + " < ");
         }
     }
-}
+
 
